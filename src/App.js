1ee55d1fe17676891464023f5c6a206eb240caf9
Emil Hausvik <emil.hausvik@uib.no>
2024-11-02 22:33:18 +0100

    new button styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
@@ -6,10 +5,12 @@ import logo from './logo.svg'; // Ensure the logo file path is correct
 
 function App() {
   const [file, setFile] = useState(null);
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("Upload your info.yaml file here");
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    setMessage(selectedFile ? `You have selected ${selectedFile.name} for upload` : "Upload your info.yaml file here");
   };
 
   const handleSubmit = async (event) => {
@@ -35,26 +36,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> {/* Logo element */}
-        <Welcome />
-        <Info />
-        <form onSubmit={handleSubmit}>
-          <input type="file" accept=".yaml, .yml" onChange={handleFileChange} />
-          <button type="submit">Upload</button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to the NMRLipids Upload API</h1>
+        <h3>{message}</h3>
+        <form onSubmit={handleSubmit} className="upload-form">
+          <input
+            id="file-upload"
+            type="file"
+            className="file-input"
+            onChange={handleFileChange}
+            style={{ display: 'none' }}
+          />
+          <button
+            type="button"
+            className="button"
+            onClick={() => document.getElementById('file-upload').click()}
+          >
+            Select file
+          </button>
+          <button type="submit" className="button">Upload</button>
         </form>
-        {message && <p>{message}</p>}
       </header>
     </div>
   );
 }
 
-function Welcome() {
-  return <h1>Welcome to the NMRLipids Info File Upload API</h1>;
-}
-
-function Info() {
-  return <h3>This allows for uploads of info.yaml files</h3>;
-}
-
 export default App;
-
